f3f81f3b01725aeb04c1e40b19fc4515da73e758
Maks <maksymilian.milosz.wrobel@gmail.com>
2025-10-26 13:08:41 +0100

    dodano alert

--- first-app/src/components/Alert.tsx
+++ first-app/src/components/Alert.tsx
@@ -2,8 +2,23 @@ import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-export const Alert = ({ children }: Props) => {
-  return <div className="alert alert-danger">{children}</div>;
+export const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
+      <button
+        onClick={onClose}
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
