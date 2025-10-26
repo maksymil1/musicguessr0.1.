bb4dc900f398d68ceff3b5d3423626d352135d5c
Maks <maksymilian.milosz.wrobel@gmail.com>
2025-10-26 19:57:08 +0100

    przyciski na srodku v.1

--- first-app/src/components/FirstPage.tsx
+++ first-app/src/components/FirstPage.tsx
@@ -3,18 +3,19 @@ import { Button } from "./Button";
 
 interface Props {
   opcje: string[];
-  klasa?: string;
   onClick: () => void;
 }
 
-export const FirstPage = ({ opcje, onClick, klasa = "strona1" }: Props) => {
+export const FirstPage = ({ opcje, onClick }: Props) => {
   return (
     <>
-      {opcje.map((opcja) => (
-        <Button klasa={klasa} key={opcja} onClick={onClick}>
-          {opcja}
-        </Button>
-      ))}
+      <div className="strona1">
+        {opcje.map((opcja) => (
+          <Button key={opcja} onClick={onClick}>
+            {opcja}
+          </Button>
+        ))}
+      </div>
     </>
   );
 };
